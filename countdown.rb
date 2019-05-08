cd2f7e69e1ab24371793b65ad19b576abc3d1a83
Michael Maggs <michaelmaggs@gmail.com>
2019-05-08 22:26:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
 def countdown
+  number = 10
+  while number > -1 
+    puts "#{number} SECOND(S)!"
+    countdown_with_sleep(1)  
+    number -= 1 
+  end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+  sleep(seconds)
 end
